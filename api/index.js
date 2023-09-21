01c4084affd2478214da225c80ec05223d059ea3
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-09-21 15:08:21 -0400

    Make transaction call asynchronous/ add try catch for Mongo connection

--- api/index.js
+++ api/index.js
@@ -7,15 +7,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
 app.get('/api/test', (req,res) => {
     res.json('test:ok2');
 });
 
-app.post('/api/transaction',(req,res) => {
+app.post('/api/transaction',async (req,res) => {
+    try{
+        console.log("Connecting to Mongo");
+
     const {name, description,datetime} = res.body;
     
 
     res.json(req.body);
+    }catch(error){
+        console.log("Error connecting to Mongo", error);
+        res.status(500).send("Internal Error");
+    }
 
 });
 
