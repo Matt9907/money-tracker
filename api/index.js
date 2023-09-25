da9f03a11f6bb4adb0e79ce9f4651a67a3d07673
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-09-25 14:21:40 -0400

    endpoint for fetching and displaying profit/losses on frontend

--- api/index.js
+++ api/index.js
@@ -32,6 +32,12 @@ app.post('/api/transaction',async (req,res) => {
 
 });
 
+app.get('/api/transactions', async (req,res) =>{
+    await mongoose.connect(process.env.MONGO_URL);
+    const transactions = await Transaction.find();
+    res.json(transactions);
+})
+
 app.listen(4000);
 
 //db pw: zNbRyJBKfepcn6kB
